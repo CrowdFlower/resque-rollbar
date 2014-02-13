ec524bf4e87ffef8f72c53380c3225499de91489
Shai Rosenfeld <srosenfeld@engineyard.com>
2014-02-13 10:25:55 -0800

    pass custom data via exception
    if you have an exception that responds to `notification_params`
    (we can change that to something else if that makes a difference), merge it
    in so it gets sent to rollbar along with payload

--- spec/resque/failure/rollbar_spec.rb
+++ spec/resque/failure/rollbar_spec.rb
@@ -11,4 +11,23 @@ describe Resque::Failure::Rollbar do
     backend = Resque::Failure::Rollbar.new(exception, worker, queue, payload)
     backend.save
   end
+
+  class CustomError < StandardError
+    attr_accessor :notifier_params
+  end
+
+  it "if exception responds to notification_params it will pass it along with the payload" do
+    exception = CustomError.new("BOOM")
+    exception.notifier_params = {'custom' => 'info'}
+    worker = Resque::Worker.new(:test)
+    queue = "test"
+    payload = {'class' => Object, 'args' => 66}
+    merged_payload = {'class' => Object, 'args' => 66, 'custom' => 'info'}
+
+    ::Rollbar = mock("rollbar")
+    ::Rollbar.should_receive(:report_exception).with(exception, merged_payload)
+
+    backend = Resque::Failure::Rollbar.new(exception, worker, queue, payload)
+    backend.save
+  end
 end
